4a401ea21450d23f47bab2832e7e9932e6f6fe68
Caitlin Potter <caitpotter88@gmail.com>
2014-04-07 20:39:19 -0400

    chore(karma): add karma test infrastructure
    Getting ready to write some tests for the doctype conversion, before working on the rest of it.
    So, adding karma as a dependency and setting up a karma configuration is a good first step.

---
+++ karma.conf.js
@@ -0,0 +1,33 @@
+function hasFlag(flag) {
+  return process.argv.indexOf(flag) >= 0;
+}
+
+module.exports = function(config) {
+  config.set({
+    port: 9018,
+    runnerPort: 9100,
+    colors: true,
+    logLevel: config.LOG_INFO,
+
+    singleRun: true,
+    autoWatch: false,
+
+    basePath: '.',
+
+    frameworks: ['jasmine'],
+
+    files: [
+      'snapdoc.js',
+      'test/*.js'
+    ],
+
+    reporters: ['dots']
+  });
+
+  if (hasFlag('--watch') || hasFlag('--autowatch')) {
+    config.set({
+      singleRun: false,
+      autoWatch: true
+    });
+  }
+};
